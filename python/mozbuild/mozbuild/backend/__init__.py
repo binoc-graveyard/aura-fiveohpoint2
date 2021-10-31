f6b93a345707595fdf7b84cbcd4c6cfe8c7d4262
Matt A. Tobin <email@mattatobin.com>
2021-10-30 21:08:58 -0400

    Revert "Issue - 3031 - Remove ChromeMap build-backend"
    This reverts commit 08e590bde76688d3fbf80b3d38bbabbc7d2bec6f.

--- python/mozbuild/mozbuild/backend/__init__.py
+++ python/mozbuild/mozbuild/backend/__init__.py
@@ -3,6 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 backends = {
+    'ChromeMap': 'mozbuild.codecoverage.chrome_map',
     'CompileDB': 'mozbuild.compilation.database',
     'RecursiveMake': 'mozbuild.backend.recursivemake',
 }
