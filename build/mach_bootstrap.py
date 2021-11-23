941a5efd84c6cf51aeb09df8dc8ae90eeca17f53
Matt A. Tobin <email@mattatobin.com>
2021-11-23 10:20:49 -0500

    Issue %3005 - Move devtools/ to components/

--- build/mach_bootstrap.py
+++ build/mach_bootstrap.py
@@ -96,7 +96,7 @@ SEARCH_PATHS = [
 # Individual files providing mach commands.
 MACH_MODULES = [
     'build/valgrind/mach_commands.py',
-    'devtools/shared/css/generated/mach_commands.py',
+    'components/devtools/shared/css/generated/mach_commands.py',
     'dom/bindings/mach_commands.py',
     'dom/media/test/external/mach_commands.py',
     'layout/tools/reftest/mach_commands.py',
