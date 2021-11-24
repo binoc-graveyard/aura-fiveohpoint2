aa68f42e5543de10406827d9f2d71f35f4071186
Moonchild <moonchild@palemoon.org>
2021-11-24 13:31:51 +0000

    Issue %3040 - Part 3: Fix whitespace.
    Fix indentation and remove TABs

--- libs/cairo/src/test-fallback-surface.h
+++ libs/cairo/src/test-fallback-surface.h
@@ -10,9 +10,9 @@
 CAIRO_BEGIN_DECLS
 
 cairo_surface_t *
-_cairo_test_fallback_surface_create (cairo_content_t	content,
-			       int		width,
-			       int		height);
+_cairo_test_fallback_surface_create (cairo_content_t    content,
+                 int        width,
+                 int        height);
 
 CAIRO_END_DECLS
 
