b97860a10f6747cecde1d88eb22bc9b79652bd02
Matt A. Tobin <email@mattatobin.com>
2022-05-05 17:11:42 -0500

    [Libs:NSS] Bump version to 3.64.1.5

--- libs/nss/src/lib/nss/nss.h
+++ libs/nss/src/lib/nss/nss.h
@@ -22,11 +22,11 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>[.<build number>]][ <ECC>][ <Beta>]"
  */
-#define NSS_VERSION "3.64.1.4" _NSS_CUSTOMIZED
+#define NSS_VERSION "3.64.1.5" _NSS_CUSTOMIZED
 #define NSS_VMAJOR 3
 #define NSS_VMINOR 64
 #define NSS_VPATCH 1
-#define NSS_VBUILD 4
+#define NSS_VBUILD 5
 #define NSS_BETA PR_FALSE
 
 #ifndef RC_INVOKED
