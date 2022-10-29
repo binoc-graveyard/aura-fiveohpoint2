ac1ad3e71e0cc8dca65a0b175cec8aef8413c23a
Matt A. Tobin <email@mattatobin.com>
2022-10-28 22:01:14 -0500

    Revert "[XPCOM:IO] Fix Base64 off-by-one issue and safeguard against this mistake in the future."
    This reverts commit dc050720a3f0eeb1ed6cde5564f3add5e28d9f06.

--- xpcom/io/Base64.cpp
+++ xpcom/io/Base64.cpp
@@ -246,7 +246,6 @@ EncodeInputStream(nsIInputStream* aInputStream,
 
 static const char kBase64URLAlphabet[] =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
-static_assert(mozilla::ArrayLength(kBase64URLAlphabet) == 0x41);
 
 // Maps an encoded character to a value in the Base64 URL alphabet, per
 // RFC 4648, Table 2. Invalid input characters map to UINT8_MAX.
@@ -268,19 +267,14 @@ static const uint8_t kBase64URLDecodeTable[] = {
   255,
   26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
   42, 43, 44, 45, 46, 47, 48, 49, 50, 51, /* a - z */
-  255, 255, 255, 255, 255
+  255, 255, 255, 255,
 };
-static_assert(mozilla::ArrayLength(kBase64URLDecodeTable) == 0x80);
 
 bool
 Base64URLCharToValue(char aChar, uint8_t* aValue) {
   uint8_t index = static_cast<uint8_t>(aChar);
-  if (index >= mozilla::ArrayLength(kBase64URLDecodeTable)) {
-    *aValue = 255;
-    return false;
-  }
-  *aValue = kBase64URLDecodeTable[index];
-  return *aValue != 255;
+  *aValue = kBase64URLDecodeTable[index & 0x7f];
+  return (*aValue != 255) && !(index & ~0x7f);
 }
 
 } // namespace
